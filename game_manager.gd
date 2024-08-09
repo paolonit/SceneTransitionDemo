2f589c796642326bae2f9a9f5dc9d18a27a33908
Tom Paoloni <tpaoloni@protonmail.com>
2024-08-09 18:30:18 -0400

    Minor code cleanup

--- game_manager.gd
+++ game_manager.gd
@@ -1,4 +1,3 @@
 extends Node
 
-
 var coin_collected = false
--- level_2.gd
+++ level_2.gd
@@ -6,7 +6,6 @@ func _on_exit_body_entered(body):
 	if body is Player:
 		Levels.change_level("level_1")
 
-
 func _on_coin_collected():
 	var portal_instance: Node2D = portal.instantiate()
 	portal_instance.position = Vector2(1000, 387)
