3b3a90057c6ddff6e69477e450442cd5057df43a
Tom Paoloni <tpaoloni@protonmail.com>
2024-08-09 15:59:42 -0400

    Fixed level call_deferred issue

--- level_1.gd
+++ level_1.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-
-
 func _on_exit_body_entered(body):
 	if body is Player:
-		get_tree().change_scene_to_file("res://level_2.tscn")
+		Levels.change_level("level_2")
---
+++ levels.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var levels = {
+	"level_1": preload("res://level_1.tscn"),
+	"level_2": preload("res://level_2.tscn")
+}
+
+func change_level(level_key):
+	get_tree().call_deferred( "change_scene_to_packed", levels[level_key])
